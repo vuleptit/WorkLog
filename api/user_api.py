8a75a1958428dc8f29a60c492e977384a1248017
miasnguyenx <miasnguyenx@gmail.com>
2023-06-14 15:06:11 +0700

    Add delete user service

--- api/user_api.py
+++ api/user_api.py
@@ -15,3 +15,8 @@ def get_user(id: int, db: Session = Depends(get_db)) -> UserBase:
 def register(user_data: UserCreate, db: Session = Depends(get_db)) -> UserBase:
     result = create_or_update_user(db=db, user_data=user_data)
     return result
+
+@router.post('/remove/{id}')
+def register(id: int, db: Session = Depends(get_db)) -> UserBase:
+    result = delete_user(db=db, user_id=id)
+    return result
--- business_rules/user_services.py
+++ business_rules/user_services.py
@@ -27,5 +27,14 @@ def create_or_update_user(db: Session, user_data: UserCreate):
     else:
         # Update
         user_in_db = get_user_by_id(user_data.id)
+        user_in_db.email = user_data.email
+        user_in_db.password = user_data.password
+        user_in_db.phone = user_data.phone
+        user_in_db.user_name = user_data.user_name
+        user_in_db.is_active = user_data.is_active
+        user_in_db.is_admin = user_data.is_admin
         return None
 
+def delete_user(db: Session, user_id: int):
+    user_query = db.query(User).where(User.id == user_id)
+    db.delete(user_query)
