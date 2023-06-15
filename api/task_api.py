a9e44cfddbc9b6c11d51d3d0eb4d4e530ae39d39
miasnguyenx <miasnguyenx@gmail.com>
2023-06-15 17:25:32 +0700

    Add CRUD for Task model

--- api/task_api.py
+++ api/task_api.py
@@ -1,23 +1,23 @@
 from fastapi import APIRouter, Depends
 
-from business_rules.user_services import *
+from business_rules.task_service import *
 from database_settings import get_db
 
 router = APIRouter()
 
 @router.get('/{id}')
-def get_task(id: int, db: Session = Depends(get_db)) -> UserBase:
-    user = get_task_by_id(db=db, user_id=id)
-    return user
+def get_task(id: int, db: Session = Depends(get_db)) -> TaskBase:
+    task = get_task_by_id(db=db, task_id=id)
+    return task
 
 
 @router.post('/new/')
-def register(user_data: UserCreate, db: Session = Depends(get_db)) -> UserBase:
-    result = create_or_update_user(db=db, user_data=user_data)
+def register(task_data: TaskBase, db: Session = Depends(get_db)) -> TaskBase:
+    result = create_or_update_task(db=db, task_data=task_data)
     return result
 
 
 @router.post('/remove/{id}')
-def register(id: int, db: Session = Depends(get_db)) -> UserBase:
-    result = delete_user(db=db, user_id=id)
+def register(id: int, db: Session = Depends(get_db)) -> TaskBase:
+    result = delete_task(db=db, task_id=id)
     return result
--- business_rules/task_service.py
+++ business_rules/task_service.py
@@ -1,4 +1,39 @@
 from sqlalchemy.orm import Session
 
-from model.user import *
-from business_rules.view_models.user_dto import *
+from model.task import *
+from business_rules.view_models.task_dto import *
+
+def get_task_by_id(db: Session, task_id: int):
+    task_query = db.query(Task).where(Task.id == task_id)
+    return task_query.first()
+
+def create_or_update_task(db: Session, task_data: TaskBase):
+    if task_data.id <= 0:
+        # Create new Task
+        task_item = Task()
+        update_fields = ["name", "project_id", "code_on_jira"]
+        for field in update_fields:
+            setattr(task_item, field, getattr(task_data, field))
+        db.add(task_item)
+        db.commit()
+        db.refresh(task_item)
+        return task_item
+    else:
+        # Update
+        task_in_db = get_task_by_id(db=db, task_id=task_data.id)
+        update_fields = ["name", "project_id", "code_on_jira"]
+        for field in update_fields:
+            setattr(task_item, field, getattr(task_data, field))
+        db.commit()
+        db.refresh(task_in_db)
+        print(type(task_in_db))
+        print(task_in_db)
+        return task_in_db
+
+def delete_task(db: Session, task_id: int):
+    task_query = db.query(Task).where(Task.id == task_id).first()
+    db.delete(task_query)
+    db.commit()
+    task_in_db = get_task_by_id(db=db, task_id=task_id)
+    return task_in_db
+    
