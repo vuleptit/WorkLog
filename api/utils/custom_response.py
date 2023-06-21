f58e5f6b33bd09983855cef6317cd98fb298e572
miasnguyenx <miasnguyenx@gmail.com>
2023-06-21 17:23:40 +0700

    Add dynamic response status code example in update user route

--- api/utils/custom_response.py
+++ api/utils/custom_response.py
@@ -1,8 +1,8 @@
 from pydantic import BaseModel
-from typing import List, Union, Dict
-
-class ResponseModel(BaseModel):
+from typing import Union
+from fastapi import status
+class CustomResponse(BaseModel):
     message: Union[str, None] = ""
-    data: Union[dict, None] = None
+    data: Union[dict, list, None] = None
     status: int
     exception: Union[str, None] = ""
--- business_rules/view_models/user_dto.py
+++ business_rules/view_models/user_dto.py
@@ -13,15 +13,23 @@ class UserBase(BaseModel):
         orm_mode = True
 
 
-class UserInDB(UserBase):
+class UserInDB(BaseModel):
+    user_name: str
     password: str
+    email: str
+    phone: str
+    is_admin: bool
+    is_active: bool
+
+    class Config:
+        orm_mode = True
 
 class UserUpdate(BaseModel):
     id: int
     user_name: str
+    password: str
     email: str
     phone: str
-    password: str
     is_active: bool
     
     class Config:
