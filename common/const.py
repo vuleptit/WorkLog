18f6021ba04024c7a2576b049e11e99a56b8e337
miasnguyenx <miasnguyenx@gmail.com>
2023-06-23 15:29:10 +0700

    Done middleware authentication + tested

--- common/const.py
+++ common/const.py
@@ -5,3 +5,7 @@ SECRET_FILE = normpath(join(PROJECT_ROOT, 'SECRET.key'))
 SECRET_KEY = open(SECRET_FILE).read().strip()
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+class MESSAGE:
+    SUCCESFUL = "Successful"
+    Exception = "Something went wrong"
---
+++ common/middleware/permission.py
@@ -0,0 +1,40 @@
+import sys
+import inspect
+from functools import wraps
+from business_rules.view_models.user_dto import *
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status
+from jose import jwt, JWTError
+from business_rules.view_models.jwt_dto import TokenData
+from common.const import *
+from business_rules.jwt_services import get_user_by_username
+
+def IsAuthenticated(func):
+    @wraps(func)
+    def is_authenticated(*args, **kwargs):
+        credentials_exception = HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+        try:
+            req = kwargs['request']
+            token_key = req.headers['authorization'].split()[1]
+            payload = jwt.decode(token_key, SECRET_KEY, algorithms=[ALGORITHM])
+            username: str = payload.get("sub")
+            if username is None:
+                raise credentials_exception
+            token_data = TokenData(username=username)
+        except JWTError:
+            print(sys.exc_info())
+            raise credentials_exception
+        except Exception as ex:
+            print(sys.exc_info())
+            raise HTTPException(detail="Not provided credentials", status_code=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION)
+        user = get_user_by_username(username=token_data.username)
+        if user is None:
+            raise 
+        if user.is_active is not True:
+            raise HTTPException(detail="User not active", status_code=status.HTTP_401_UNAUTHORIZED)
+        return func(*args, **kwargs)
+    return is_authenticated
